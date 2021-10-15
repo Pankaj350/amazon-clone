c378eaac09c84e4492e3009a5d48aeb51b7cab5b
Pankaj kannaujiya <kannaujiyapankaj590@gmail.com>
2021-10-15 22:41:46 +0530

    init

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import './Checkout.css';
+import Subtotal from './Subtotal';
 
 function Checkout() {
     return (
         <div className="checkout">
 
             <div className="checkout_left">
-               <img className="checkout_adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
+                <img className="checkout_adv" src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Events/Jupiter21P1/pay_stripe_desk.png" alt="adv" />
 
-              <div className="checkout_title">
-                  <h3>Your Shoping Details</h3>
-              </div>
+                <div className="checkout_title">
+                    <h3>Your Shoping Details</h3>
+                </div>
             </div>
 
             <div className="checkout_right">
-            <div>
-                  <h3>SubTotal (2 item)</h3>
-              </div>
+                <div>
+                    <Subtotal />
+                </div>
             </div>
-            
+
         </div>
     );
 }
